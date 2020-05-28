8cd3df3b523c0c1f061d53fee9f32db545e9bf40
Alediran <55855331+Alediran@users.noreply.github.com>
2020-05-28 17:16:41 -0300

    Generic View

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/Users';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
 
-  userForm.render();
+  userEdit.render();
 } else {
   throw new Error('Root element not found');
 }
---
+++ src/views/UserShow.ts
@@ -0,0 +1,13 @@
+import { User, UserProps } from '../models/Users';
+import { View } from './View';
+
+export class UserShow extends View<User, UserProps> {
+  template(): string {
+    return `
+    <div>
+      <h1>User Detail</h1>
+      <div>Use Name: ${this.model.get('name')}</div>
+      <div>Age: ${this.model.get('age')}</div>
+    </div>`;
+  }
+}
